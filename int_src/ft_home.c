b146da610dade23668d83d70ea1fb7995268c8f1
Mathieu MOLE <mmole@e1r4p14.42.fr>
2014-03-01 20:36:01 +0100

    home et end

---
+++ int_src/ft_home.c
@@ -0,0 +1,85 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_home.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmole <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2014/03/01 13:14:49 by mmole             #+#    #+#             */
+/*   Updated: 2014/03/01 20:33:08 by mmole            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <term.h>
+#include "../includes/ft_minishell.h"
+#include "../libft/libft.h"
+
+int		ft_poscurseur(t_edit **lst)
+{
+	t_edit	*tmp;
+	int		i;
+	int		Col;
+
+	i = 0;
+	Col = tgetnum("co");
+	tmp = *lst;
+	while (!(tmp->video) && tmp->next)
+	{
+		i++;
+		tmp = tmp->next;
+	}
+	if (tmp->next)
+		i = i - 1;
+	if (i > 0)
+		return (i);
+	return (0);
+}
+
+void	ft_home(t_edit **lst)
+{
+	int	pos;
+	char	key[4];
+
+	pos = ft_poscurseur(lst);
+	key[0] = 27;
+	key[1] = 91;
+	key[2] = 68;
+	key[3] = 0;
+	if (pos != 0)
+	{
+		while ((pos) >= 0)
+		{
+			arrow_left_right(lst, key);
+			pos--;
+		}
+	}
+}
+
+void	ft_end(t_edit **lst)
+{
+	int		pos;
+	char	key[4];
+	t_edit	*tmp;
+	int		max;
+
+	max = 0;
+	tmp = *lst;
+	while (tmp)
+	{
+		tmp = tmp->next;
+		max++;
+	}
+	pos = ft_poscurseur(lst);
+	key[0] = 27;
+	key[1] = 91;
+	key[2] = 67;
+	key[3] = 0;
+	if (pos != 0)
+	{
+		while ((pos) < max)
+		{
+			arrow_left_right(lst, key);
+			pos++;
+		}
+	}
+}
