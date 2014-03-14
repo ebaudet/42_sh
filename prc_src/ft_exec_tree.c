5644c2f287adc27ca96256b8732573ca6233e1f9
Wouterbeets <beetswouter@gmail.com>
2014-03-14 16:02:38 +0100

    boo

---
+++ prc_src/ft_exec_tree.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_exec_tree.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wbeets <wbeets@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2014/03/14 09:05:27 by wbeets            #+#    #+#             */
+/*   Updated: 2014/03/14 16:01:41 by wbeets           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "prc.h"
+
+int		ft_exec_tree(t_op **tree, char **env)
+{
+	int		ret;
+
+	if (((*tree)->lft && (*tree)->lft->stat == 1)
+		|| (*tree)->rgt && (*tree)->rgt->stat == 1)
+	{
+		(*tree)->stat = 0;
+		ft_create_op(tree);
+		if ((*tree)->lft && (*tree)->lft->stat)
+			ret = ft_exec_tree(&(*tree)->lft, env);
+		else if ((*tree)->rgt &&(*tree)->rgt->stat)
+			ret = ft_exec_tree(&(*tree)->rgt, env);
+		if ((*tree)->code == COM)
+			ret = ft_execve((*tree)->name, (*tree)->argv, env);
+	}
+	if ((*tree)->top)
+		ret = ft_exec_tree(&(*tree)->top, env);
+}
