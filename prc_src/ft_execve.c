21c1c97fe1f70c65b0ef5a02773fb91bfd38f7d2
Wouterbeets <beetswouter@gmail.com>
2014-03-17 18:39:53 +0100

    boo

---
+++ prc_src/ft_execve.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_execve.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wbeets <wbeets@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2014/03/13 14:41:35 by wbeets            #+#    #+#             */
+/*   Updated: 2014/03/13 15:16:10 by wbeets           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include "libft.h"
+
+int		ft_exceve(char *cmd, char **argv, char **env)
+{
+	pid_t	fork_return;
+	char	**paths;
+	char	*tmp;
+	int		i;
+	int		ret;
+
+	i = -1;
+	if ((ret = ft_builtin(cmd, argv, env) == -1))
+	{
+		fork_return = fork();
+		if (fork_return == 0)
+		{
+			paths = ft_get_paths(env);
+			ft_putstr("in_child");
+			while (paths[++i] != '\0')
+			{
+				tmp = ft_strjoin(paths[i], cmd);
+				execve(tmp, argv, env);
+				free(tmp);
+			}
+		}
+		else
+		{
+			waitpid(fork_return, &ret);
+		}
+	}
+	return (ret);
+}
