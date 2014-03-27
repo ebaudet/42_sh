d95d4e97c86a964c981ec87aef227eb818500983
Wouterbeets <beetswouter@gmail.com>
2014-03-27 16:03:24 +0100

    bo

--- int_src/ft_env.c
+++ int_src/ft_env.c
@@ -6,7 +6,7 @@
 /*   By: ebaudet <ebaudet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/03/03 21:13:08 by ebaudet           #+#    #+#             */
-/*   Updated: 2014/03/14 00:42:42 by ebaudet          ###   ########.fr       */
+/*   Updated: 2014/03/27 14:54:33 by wbeets           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,7 +41,7 @@ char		**ft_get_path(char **env)
 	if ((tmp = ft_strsplit(getenv("PATH"), ':')) == NULL)
 		return (NULL);
 	i = ft_count_c(getenv("PATH"), ':') + 1;
-	if ((path = (char **)malloc(i * sizeof(char *))) == NULL)
+	if ((path = (char **)malloc((i + 1) * sizeof(char *))) == NULL)
 		return (NULL);
 	j = 0;
 	while (j < i)
